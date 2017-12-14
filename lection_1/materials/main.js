9d44b66f3800936425f60f5a7a9c66eebc44358a
SychovSaveliy <savelii.sychov@gettyimages.com>
2017-12-14 17:02:52 +0200

    Lection_1

---
+++ lection_1/materials/main.js
@@ -0,0 +1,25 @@
+// a = 10;
+// b = 20;
+
+// console.log(a, b, 'a + b');
+
+// // x = a - b;
+// x = "hello"; 
+// console.log(x);
+// x = a / b;
+// console.log(x);
+
+// x = a * b;
+// console.log(x);
+
+// x = a % b; // остаток от деления
+// console.log(x);
+// // number
+// string
+
+a = 10;
+b = '2ss0';
+// b = parseInt(b);
+
+console.log( a + (+b) );
+
