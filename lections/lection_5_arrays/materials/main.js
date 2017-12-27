6203d87bc28685e981c57643db32872ccb33d039
SychovSaveliy <savelii.sychov@gettyimages.com>
2017-12-27 21:04:34 +0200

    update

--- lections/lection_5_arrays/materials/main.js
+++ lections/lection_5_arrays/materials/main.js
@@ -1,5 +1,5 @@
-n = 10;
-m = 20;
+n = 5;
+m = 5;
 
 A = new Array(n);
 
@@ -24,12 +24,40 @@ console.log(A);
 
 // сумма непарных столбцов
 
+// for(i = 0, sum = 0; i < A.length; i++){
+//     for(j = 0; j < A[i].length; j++){
+//       if(j % 2 != 0) {
+//         sum += A[i][j];
+//       }
+//     }
+// }
+
+// console.log(sum);
+
+
+
+
+var tA = new Array(A.length);
+
+for(i = 0; i < tA.length; i++){
+    tA[i] = new Array(A[i].length);
+}
+
+
 for(i = 0, sum = 0; i < A.length; i++){
     for(j = 0; j < A[i].length; j++){
-      if(j % 2 != 0) {
-        sum += A[i][j];
-      }
+      tA[j][i] = A[i][j];
     }
 }
 
-console.log(sum);
+
+document.write('<hr />tA<hr />');
+for(i = 0; i < tA.length; i++){
+    for(j = 0; j < tA[i].length; j++){
+        // console.log(A[i][j]);
+
+        
+        document.write(tA[i][j] + " ");
+    }
+    document.write("<br/>");
+}
