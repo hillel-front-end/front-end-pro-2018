0ca586b36eb9459ff1ef2e39af917a5db248885c
SychovSaveliy <savelii.sychov@gettyimages.com>
2018-01-28 13:58:02 +0200

    Lection_11

---
+++ lections/lection_11_OOP/materials/main.js
@@ -0,0 +1,29 @@
+console.log('Lection 11');
+
+function Human(name, age, speed) {
+    this.name = name;
+    this.age = age;
+    this.speed = speed;
+
+    // this.run = function() {
+    //     console.log(this.name + " run" + '_' + this.speed);
+    // }
+
+}
+
+// Human.prototype.foo = 200;
+Human.prototype.run = function() {
+    console.log(this.name + " run" + '_' + this.speed);
+}
+
+Human.prototype.clear = function() {
+    this.speed = 0;
+}
+
+console.dir(Human);
+
+
+var p1 = new Human('Vasya', 23, 25);
+var p2 = new Human('Petya', 65, 10);
+
+p1.run();
