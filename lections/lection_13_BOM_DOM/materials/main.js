4babc90f257ffd00aa1a82a7f7cbcb8d421a5cdd
SychovSaveliy <savelii.sychov@gettyimages.com>
2018-02-04 14:22:50 +0200

    Lection_13

---
+++ lections/lection_13_BOM_DOM/materials/main.js
@@ -0,0 +1,62 @@
+/*
+
++ Глобальные обьекты Window, Document, Navigator, Location
++ JS обращение в html
++ html коллекция
++ html элемент
++ Обьекты classList, className
+
+*/
+
+window.onload = function() {
+    // var block = document.getElementById('block');
+    // getElemenetsByClassName
+    // getElementsByTagName
+
+    // ---------------
+
+    //querySelector
+    //querySelectorAll
+
+    // var block = document.querySelector('#block');
+    // console.dir(block);
+
+    // var elems = document.querySelectorAll('#block li');
+    // console.log(elems);
+
+    // for(var i = 0; i < elems.length; i++){
+    //     elems[i].style.border = "2px solid black";
+    // }
+
+    // classList
+    // className
+    // style
+
+
+    var elem = document.querySelector('.element');
+    console.dir(elem);
+    window.elem = elem;
+    // elem.className += " block";
+
+    elem.style.top = "250px";
+    elem.style.left = "100px";
+    elem.style.backgroundColor = "blue";
+    elem.style.transition = ".1s";
+
+    elem.classList.add('block', 'p1', 'f2');
+    elem.classList.remove('three');
+
+    var direction = 1;
+    // var step = 5;
+    setInterval(function(){
+        var top = parseInt(elem.style.top);
+        var step = step || Math.floor(Math.random()*10);
+        elem.style.top = top + (step*direction) + "px";
+    }, 200);
+
+    setInterval(function(){
+        direction = -direction;
+    }, 2000)
+}
+
+
