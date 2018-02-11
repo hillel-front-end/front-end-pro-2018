d0290d977a0e9b5c4ad73e172999d63d79daf477
SychovSaveliy <savelii.sychov@gettyimages.com>
2018-02-11 14:37:49 +0200

    upd

--- lections/lection_15_DOM_events/materials/main.js
+++ lections/lection_15_DOM_events/materials/main.js
@@ -27,7 +27,7 @@ window.onload = function() {
     //     console.log('2', event.type, event);
     // });
 
-    block2.addEventListener('click', onClickBl2);
+    // block2.addEventListener('click', onClickBl2);
 
     // setTimeout(function(){
     //     block2.removeEventListener('click', onClickBl2);
@@ -38,7 +38,74 @@ window.onload = function() {
     // }, 5000);
 
 
-    function onClickBl2(event){
-        console.log('2', event.type, event);
+    // function onClickBl2(event){
+    //     console.log('2', event.type, event);
+    // }
+    window.dragDrop = {
+        startX: null,
+        startY: null,
+        mouseStartX: null,
+        mouseStartY: null
+    };
+
+    window.block = block;
+
+    // block.addEventListener('mousedown', onMouseDown);
+    // block.addEventListener('mouseup', onMouseUp);
+    // document.addEventListener('mouseup', onMouseUp);
+
+    var btn = document.querySelector('.btn'),
+        flag = true,
+        interval;
+
+    btn.onclick = function() {
+        if (flag){
+            clearInterval(interval);
+            flag = false;
+        } else{
+            setAnimation();
+        }
+    }
+    
+    setAnimation();
+
+    function setAnimation() {   
+        block.addEventListener('mousedown', function(){
+            flag = true;
+            btn.click();
+        });
+
+        block.addEventListener('mouseup', function(){
+            flag = false;
+            btn.click();
+        });
+
+        interval = setInterval(function(){
+            block.style.left = Math.floor(Math.random()*window.innerWidth) + 'px';
+            block.style.top = Math.floor(Math.random()*window.innerHeight) + 'px';
+        }, 1000);
     }
 }
+
+
+function onMouseDown(event) {
+    dragDrop.startX = event.target.offsetLeft;
+    dragDrop.startY = event.target.offsetTop;
+    dragDrop.mouseStartX = event.clientX;
+    dragDrop.mouseStartY = event.clientY; 
+
+    document.addEventListener('mousemove', onMove);
+}
+
+function onMouseUp(event) {
+    document.removeEventListener('mousemove', onMove);
+}
+
+function onMove(event){
+    var  x = event.clientX,
+         y = event.clientY; 
+
+    block.style.left = dragDrop.startX + (x - dragDrop.mouseStartX) + 'px';
+    block.style.top = dragDrop.startY + (y - dragDrop.mouseStartY) + 'px';
+}
+
